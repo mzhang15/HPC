40a6ea2b70d8425ef2375c76822ab8cfbb1aec71
Mengyang Zhang <mengyangzhang@10-16-220-67.dynapool.wireless.nyu.edu>
2021-03-27 12:17:08 -0400

    hw3 finished p2 and work on p3

---
+++ hw3/omp_solved_bug6.c
@@ -0,0 +1,47 @@
+/******************************************************************************
+* FILE: omp_bug6.c
+* DESCRIPTION:
+*   This program compiles and runs fine, but produces the wrong result.
+*   Compare to omp_orphan.c.
+* AUTHOR: Blaise Barney  6/05
+* LAST REVISED: 06/30/05
+* COMMENT: If we have #pragma omp parallel directive outside the dotprod function, the variable sum declared in dotprod is private to the thread, while the reduction directive expects it to beshared among threads. Thus, we need to move the parallel directive inside the dotprod function.
+******************************************************************************/
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define VECLEN 100
+
+float a[VECLEN], b[VECLEN];
+
+float dotprod ()
+{
+int i,tid;
+float sum;
+
+tid = omp_get_thread_num();
+#pragma omp parallel for reduction(+:sum)
+  for (i=0; i < VECLEN; i++)
+    {
+    sum = sum + (a[i]*b[i]);
+    printf("  tid= %d i=%d\n",tid,i);
+    }
+return sum;
+}
+
+
+int main (int argc, char *argv[]) {
+int i;
+float sum;
+
+for (i=0; i < VECLEN; i++)
+  a[i] = b[i] = 1.0 * i;
+sum = 0.0;
+
+// #pragma omp parallel shared(sum)
+sum = dotprod();
+
+printf("Sum = %f\n",sum);
+
+}
+
