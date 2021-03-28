23d3c3c9be8476e9164c41f6bfe9d83bb4f68467
Mengyang Zhang <mz2325@nyu.edu>
2021-03-28 10:26:04 -0400

    implemented OpenMP version of omp-can but still slow

--- hw3/omp-scan.cpp
+++ hw3/omp-scan.cpp
@@ -15,6 +15,48 @@ void scan_seq(long* prefix_sum, const long* A, long n) {
 
 void scan_omp(long* prefix_sum, const long* A, long n) {
   // TODO: implement multi-threaded OpenMP scan
+  int nthreads, block_len;
+  #pragma omp parallel
+  {
+    int tid = omp_get_thread_num();
+    if (tid == 0) {
+      nthreads = omp_get_num_threads();
+      block_len = n / nthreads;
+      printf("Number of threads = %d\n", nthreads);
+    }
+
+    // Q: why adding a barrier here would cause compiling error?
+    // #pragma omp barrier
+    // printf("Thread %d is starting...\n", tid)
+    #pragma omp barrier
+   
+    // 1. split A into chuncks and do local scan
+    #pragma omp for
+    for (long i = 0; i < n; ++i) {
+      if (i % block_len == 0) prefix_sum[i] = 0;
+      else prefix_sum[i] = prefix_sum[i - 1] + A[i - 1];   
+    }
+  }
+
+  // 2. calculate offsets
+  long offsets[nthreads];
+  long sum = 0;
+  for (long i = 0; i < n; ++i) {
+    if (i % block_len == 0) offsets[i / block_len] = sum;
+    sum += A[i];
+  }
+  
+
+  #pragma omp parallel
+  {
+    int tid = omp_get_thread_num();
+    // 3. update prefixSum with offset
+    #pragma omp for
+    for (long i = 0; i < n; ++i) {
+      prefix_sum[i] += offsets[i / block_len];
+    }
+    printf("Thread %d is done!\n", tid);
+  }
 }
 
 int main() {
