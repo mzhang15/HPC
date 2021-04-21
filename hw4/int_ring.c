690862329097a78f85cd0d9eff14b68722b1b6d7
Mengyang Zhang <mz2325@log-1.nyu.cluster>
2021-04-20 23:43:43 -0400

    updated int_ring.c

--- hw4/int_ring.c
+++ hw4/int_ring.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <mpi.h>
 #include <string.h>
 
@@ -13,34 +14,41 @@ unsigned int time_int_ring(long Nrepeat, long Nsize, MPI_Comm comm, double* time
 	double tt = MPI_Wtime();
 
 	for (long repeat = 0; repeat < Nrepeat; ++repeat) {
+		printf("started repeat %d\n", repeat);
 		MPI_Status status;
 
 		switch(rank) {
 			case 0:
 				if (repeat == 0) memcpy(msg, (char*)&sum,sizeof(unsigned int)); // put sum = 0 into msg
-				else MPI_Recv(msg, Nsize, MPI_CHAR, 2, comm, &status);
-
-				MPI_Send(msg, Nsize, MPI_CHAR, 1, comm);
+				else MPI_Recv(msg, Nsize, MPI_CHAR, 2, repeat, comm, &status);
+				
+				MPI_Send(msg, Nsize, MPI_CHAR, 1, repeat, comm);
+				printf("process %d: sum = %d, repeat = %d\n", rank, sum, repeat);
 				break;
 			case 1:
-				MPI_Recv(msg, Nsize, MPI_CHAR, 0, comm, &status);
+				MPI_Recv(msg, Nsize, MPI_CHAR, 0, repeat, comm, &status);
 
 				sum = *(unsigned int*)(msg);
 				sum += 1;
 				memcpy(msg, (char*)&sum,sizeof(unsigned int));
 
-				MPI_Send(msg, Nsize, MPI_CHAR, 2, comm);
+				MPI_Send(msg, Nsize, MPI_CHAR, 2, repeat, comm);
+				printf("process %d: sum = %d, repeat = %d\n", rank, sum, repeat);
 				break;
 			case 2:
-				MPI_Recv(msg, Nsize, MPI_CHAR, 1, comm, &status);
+				MPI_Recv(msg, Nsize, MPI_CHAR, 1, repeat, comm, &status);
 
 				sum = *(unsigned int*)(msg);
 				sum += 2;
 				memcpy(msg, (char*)&sum,sizeof(unsigned int));
 
-				MPI_Send(msg, Nsize, MPI_CHAR, 0, comm);
-				break
+				MPI_Send(msg, Nsize, MPI_CHAR, 0, repeat, comm);
+				printf("process %d: sum = %d, repeat = %d\n", rank, sum, repeat);
+				break;
+			default:
+				break;
 		}
+		printf("finished repeat %d\n", repeat);
 	}
 
 	tt = MPI_Wtime() - tt;
@@ -64,7 +72,7 @@ int main(int argc, char* argv[]) {
 
 	double tt;
 	unsigned int result = time_int_ring(Nrepeat, 8, comm, &tt);
-	if (rank == 0) printf("int ring latency: %e ms\n", *tt/Nrepeat * 1000);
+	if (rank == 0) printf("int ring latency: %e ms\n", tt/Nrepeat * 1000);
 	printf("result: %d\n", result);
 
 	MPI_Finalize();
